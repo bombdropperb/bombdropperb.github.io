48d6d6f2871f9b1ef5c85e9a695138081b5da823
steveny9911 <stevenywy@gmail.com>
2019-05-23 21:05:28 -0700

    Fix CSS & Fix video

--- start.js
+++ start.js
@@ -4,11 +4,9 @@ var imageNormal = document.getElementById('image-normal');
 var imageMuda = document.getElementById('image-muda');
 
 var punching = document.getElementById("video-punching");
-var overlay = document.getElementById("overlay")
 
-var clicks = 0;
 var counter = 0;
-var isVidRunning = 0
+var isVidRunning = false;
 
 var audio = new Audio('sound.mp3');
 
@@ -18,28 +16,35 @@ function toggleImage(down) {
     imageMuda.style.display = down ? 'block' : 'none';
 }
 
-// actual coutner
 function ar(i) {
-    if (isVidRunning)
+    if (isVidRunning) {
         return;
-    toggleImage(counter++ % 2);
+    }
 
-    if (counter % 6 == 0) {
-        isVidRunning = 1;
+    toggleImage(i);
+
+    if (++counter % 12 == 0) {
+        isVidRunning = true;
         imageNormal.style.display = 'none';
         imageMuda.style.display = 'none';
         punching.style.display = 'block';
-        // toggleImage(true);
         punching.play();
     }
 
 }
 
+punching.onended = function () {
+    imageNormal.style.display = 'block';
+    imageMuda.style.display = 'none';
+    punching.style.display = 'none';
+    isVidRunning = false;
+};
+
 toggleImage(false);
 
-root.addEventListener('mousedown', function(a) {if(a.button !== 0) return; ar(a); });
-root.addEventListener('mouseup', function(a) {if(a.button !== 0) return; ar(a); });
+root.addEventListener('mousedown', function(a) {if(a.button !== 0) return; ar(true);});
+root.addEventListener('mouseup', function(a) {if(a.button !== 0) return; ar(false);});
+
 root.addEventListener('touchstart', function(e) { ar(a); e.preventDefault(); });
-// root.addEventListener('touchmove', function(e) { e.preventDefault(); });
 root.addEventListener('touchend', function(e) { ar(a); });
 root.addEventListener('touchcancel', function(e) { ar(a); });
